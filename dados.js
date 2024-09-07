ef2e87bb56236d9ac440b6f8fc889e9d6286ac5e
Gvcustodio <gvcustodio@bol.com.br>
2024-09-07 10:11:42 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,38 @@
+let dados = [
+  {
+    titulo: "O Senhor dos Anéis: A Sociedade do Anel",
+    descricao: "Uma épica jornada de um hobbit e seus companheiros para destruir um poderoso anel e salvar a Terra Média de um lorde das trevas.",
+    faixaEtaria: "12+",
+    link: "https://pt.wikipedia.org/wiki/O_Senhor_dos_Anéis:_A_Sociedade_do_Anel"
+  },
+  {
+    titulo: "Interestelar",
+    descricao: "Um grupo de astronautas embarca em uma missão através de um buraco de minhoca, em busca de um novo lar para a humanidade.",
+    faixaEtaria: "13+",
+    link: "https://pt.wikipedia.org/wiki/Interestelar"
+  },
+  {
+    titulo: "Coringa",
+    descricao: "Uma exploração sombria da origem do icônico vilão da DC Comics, mostrando a ascensão de um homem marginalizado à loucura.",
+    faixaEtaria: "18+",
+    link: "https://pt.wikipedia.org/wiki/Coringa_(filme_de_2019)"
+  },
+  {
+    titulo: "Parasita",
+    descricao: "Uma sátira social que explora as disparidades de classe através da história de duas famílias coreanas.",
+    faixaEtaria: "16+",
+    link: "https://pt.wikipedia.org/wiki/Parasita"
+  },
+  {
+    titulo: "Vingadores: Ultimato",
+    descricao: "O clímax da saga dos Vingadores, onde os heróis mais poderosos da Terra se unem para reverter os danos causados por Thanos.",
+    faixaEtaria: "12+",
+    link: "https://pt.wikipedia.org/wiki/Vingadores:_Ultimato"
+  },
+  {
+    titulo: "Soul",
+    descricao: "Uma animação da Pixar que explora questões existenciais sobre a vida, a morte e a busca por um propósito.",
+    faixaEtaria: "Todas as idades",
+    link: "https://pt.wikipedia.org/wiki/Soul_(filme)"
+  }
+];
